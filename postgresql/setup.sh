68750c647f9cf76e6d4c9943688f43c8e2880a1f
SatuElisa <elisa.schaeffer@gmail.com>
2022-08-18 09:41:34 -0400

    Update setup.sh

--- postgresql/setup.sh
+++ postgresql/setup.sh
@@ -1,3 +1,6 @@
 sudo apt update
 sudo apt install postgresql postgresql-contrib
 sudo systemctl start postgresql.service
+# we will run this as a special postgres user
+#
+#          sudo -u postgres psql
