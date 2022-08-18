05c3f5cbebcf45a169c867a4c837718f367da49f
SatuElisa <elisa.schaeffer@gmail.com>
2022-08-18 09:37:03 -0400

    Update setup.sh

--- mariadb/setup.sh
+++ mariadb/setup.sh
@@ -8,5 +8,8 @@ sudo apt install mariadb-server
 # there may be additional questions involved in replacing existing installations                                                      
 sudo systemctl start mariadb.service
 sudo mysql_secure_installation # no need to change your password, but                                                                 
-			                         # DO disable the remote/test stuff and                                                                 
+			       # DO disable the remote/test stuff and                                                                 
                                # then reload                                                                                          
+# we can now rerun dump.sql from ../mysql/ 
+# without needing to take any of the extra steps explained in comments
+# since we created /vault/ and placed the files there when working with mysql
