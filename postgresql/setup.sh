e8163b62b47908ff09f8037b1871609608955298
SatuElisa <elisa.schaeffer@gmail.com>
2022-08-18 09:43:38 -0400

    Update setup.sh

--- postgresql/setup.sh
+++ postgresql/setup.sh
@@ -4,3 +4,11 @@ sudo systemctl start postgresql.service
 # we will run this as a special postgres user
 #
 #          sudo -u postgres psql
+# 
+# while in, create our example DB using
+#
+#          createdb school
+#
+# and then exit by typing
+#
+#          \q
