e093e62ef36c7f0cb1775e100cd1a61b07c28bcd
SatuElisa <elisa.schaeffer@gmail.com>
2022-08-18 08:45:23 -0400

    Starting with mysql for S4

---
+++ mysql/setup.sh
@@ -0,0 +1,6 @@
+# for debian-type linux (including ubuntu)
+# note: you need admin rights (meaning: you need to belong to the sudoers group)                                                          
+sudo apt update
+sudo apt upgrade # update any packages already installed before adding new ones                                                           
+sudo apt install mysql-server
+sudo mysql -u root -p # the password will be blank 
