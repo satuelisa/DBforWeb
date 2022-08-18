bfdc83968a880667ba3938a0bcdafc9b72adfd9e
SatuElisa <elisa.schaeffer@gmail.com>
2022-08-18 09:58:51 -0400

    Add files via upload

---
+++ postgresql/check.sql
@@ -0,0 +1,7 @@
+/* place this file into /vault/ and then execute as 
+
+        sudo -u postgres psql -d school -a -f check.sql
+
+to run in batch mode */
+SELECT * FROM students;
+SELECT * FROM grades;
