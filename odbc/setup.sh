412ddd3f465e67bb33404f2e7a716bba31f01a1d
SatuElisa <elisa.schaeffer@gmail.com>
2022-10-01 11:03:13 -0400

    added compilation and execution

--- odbc/setup.sh
+++ odbc/setup.sh
@@ -1,2 +1,4 @@
 sudo apt install cl-sql-sqlite3 sqlitebrowser
 wget https://github.com/xerial/sqlite-jdbc/releases/download/3.39.3.0/sqlite-jdbc-3.39.3.0.jar
+javac -classpath sqlite-jdbc-3.39.3.0.jar Test.java
+java Test
